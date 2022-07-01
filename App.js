9fe4232003408e17ddcd80a06116ee409cd4bc4f
Roman Tsuhulov <84708720+Sigul4@users.noreply.github.com>
2022-07-01 15:28:19 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Episodes from './Episodes';
+import Persons from './Persons';
+
+function App() {
+  return (
+    <>
+      <Persons />
+      <Episodes />
+    </>
+  );
+}
+
+export default App;
